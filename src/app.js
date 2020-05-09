bc39653cbc116ea42218e70bcb59185acb097197
James Whayman <james@wildpress.co>
2020-05-09 15:44:07 +0100

    styling updates

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ let App = {
 
         // Replace the placeholder for location search fields
         $('.form-control__google-location').attr('placeholder', 'Base Location (Optional)');
+
+        // Add link to search tips
+        $('.quick-search .form-control__google-location').parent().append('<a href="/blog/employers-guide-to-remotejobs/" id="search-tips" target="_blank">Search tips</a>');
     }
 };
 
