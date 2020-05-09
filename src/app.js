cf6c8ec5e8acdeff482397bf67a54c2252b5361a
James Whayman <james@wildpress.co>
2020-05-09 16:40:44 +0100

    styling updates

--- src/app.js
+++ src/app.js
@@ -5,8 +5,11 @@ let App = {
         // Replace the placeholder for location search fields
         $('.form-control__google-location').attr('placeholder', 'Base Location (Optional)');
 
-        // Add link to search tips
-        $('.quick-search .form-control__google-location').parent().append('<a href="/blog/employers-guide-to-remotejobs/" id="search-tips" target="_blank">Search tips</a>');
+        // Add search tips for job seekers link
+        $('.quick-search .form-control__google-location').parent().append('<a href="/blog/job-seekers-guide-to-remotejobs/" id="search-tips" target="_blank">Search tips</a>');
+
+        // Add search tips for employers link
+        $('#add-listing-form [data-field="custom_field_HowRemote_2028"]').append('<a href="/blog/employers-guide-to-remotejobs/" id="search-tips" target="_blank">Read our job posting tips</a>');
     }
 };
 
