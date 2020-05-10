ed7593dcd72158de57230a18e6d3022f703e40d2
James Whayman <james@wildpress.co>
2020-05-10 11:37:29 +0100

    styling updates

--- src/app.js
+++ src/app.js
@@ -3,13 +3,26 @@ let App = {
         console.log('initializing remote jobs');
 
         // Replace the placeholder for location search fields
-        $('.form-control__google-location').attr('placeholder', 'Base Location (Optional)');
+        if ($('.form-control__google-location').length) {
+            $('.form-control__google-location').attr('placeholder', 'Base Location (Optional)');
+        }
 
         // Add search tips for job seekers link
-        $('.quick-search .form-control__google-location').parent().append('<a href="/blog/job-seekers-guide-to-remotejobs/" id="search-tips" target="_blank">Search tips</a>');
+        if ($('.quick-search .form-control__google-location').length) {
+            $('.quick-search .form-control__google-location').parent().append('<a href="/blog/job-seekers-guide-to-remotejobs/" id="search-tips" target="_blank">Search tips</a>');
+        }
 
         // Add search tips for employers link
-        $('#add-listing-form [data-field="custom_field_HowRemote_2028"]').append('<a href="/blog/employers-guide-to-remotejobs/" id="search-tips" target="_blank">Read our job posting tips</a>');
+        if ($('#add-listing-form [data-field="custom_field_HowRemote_2028"]').length) {
+            $('#add-listing-form [data-field="custom_field_HowRemote_2028"]').append('<a href="/blog/employers-guide-to-remotejobs/" id="search-tips" target="_blank">Read our job posting tips</a>');
+        }
+
+        // Activate the remote base location setting
+        if ($('#location-remote').length) {
+            $('#location-remote').click();
+        }
+
+        // Hide onsite role option
     }
 };
 
