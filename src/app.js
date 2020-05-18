b01a5aec6d9e6e99f9a5b409e7cebc5ba7cb3dc8
James Whayman <james@wildpress.co>
2020-05-18 16:36:35 +0100

    styling updates

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 let App = {
     init() {
-        console.log('initializing remote jobs');
-
         // Replace the placeholder for location search fields
         if ($('.form-control__google-location').length) {
             $('.form-control__google-location').attr('placeholder', 'Base Location (Optional)');
@@ -17,12 +15,15 @@ let App = {
             $('#add-listing-form [data-field="custom_field_HowRemote_2028"]').append('<a href="/blog/employers-guide-to-remotejobs/" id="search-tips" target="_blank">Read our job posting tips</a>');
         }
 
+        // Add search tips for employers link
+        if ($('#editListingForm [data-field="custom_field_HowRemote_2028"]').length) {
+            $('#editListingForm [data-field="custom_field_HowRemote_2028"]').append('<a href="/blog/employers-guide-to-remotejobs/" id="search-tips" target="_blank">Read our job posting tips</a>');
+        }
+
         // Activate the remote base location setting
         if ($('#location-remote').length) {
             $('#location-remote').click();
         }
-
-        // Hide onsite role option
     }
 };
 
